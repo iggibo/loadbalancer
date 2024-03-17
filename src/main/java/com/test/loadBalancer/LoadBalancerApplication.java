9183436f6c92f61bcf104508fcd1667d588a02d4
Nacho <i.gimenez.botella@gmail.com>
2024-03-17 18:54:59 +0100

    Balance RoundRobin with mod operation

--- src/main/java/com/test/loadBalancer/LoadBalancerApplication.java
+++ src/main/java/com/test/loadBalancer/LoadBalancerApplication.java
@@ -6,8 +6,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoadBalancerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoadBalancerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LoadBalancerApplication.class, args);
+        loadBalance();
+    }
 
+    private static void loadBalance() {
+        doGetServer(new RoundRobin());
+    }
+
+    private static void doGetServer(LoadBalance loadBalance) {
+        doGetServer(loadBalance, 20);
+    }
+
+    private static void doGetServer(LoadBalance loadBalance, int queryTimes) {
+        for (int i = 0; i < queryTimes; i++) {
+            String serverId = loadBalance.getServer(String.valueOf(i));
+            System.out.printf("[%s] index:%s,%s%n", loadBalance.getClass().getSimpleName(), i, serverId);
+        }
+    }
 }
